ba1c02a18c56b21fe7c2c2c33fc67a8fb1f4ca6c
Suryanshushah99 <56834606+Suryanshushah99@users.noreply.github.com>
2019-10-22 15:47:14 +0530

    Create greater.cpp

---
+++ greater.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+template <class t1,class t2,class t3>
+
+
+t3 greater_number(t1 x,t2 y)
+{
+	if(x>y)
+	return x;
+	else
+	return y;
+}
+
+
+int main()
+{
+ x=greater_number(10,9.67)<<endl;
+	cout<<t3;
+
+	
+	
+	
+}
